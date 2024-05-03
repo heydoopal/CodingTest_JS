923add559162a71f45e5fb37ea3e0535e252f24f
zidoopal <167097136+heydoopal@users.noreply.github.com>
2024-05-03 10:59:41 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -1,11 +1,11 @@
 function solution(price) {
+  let ans = price;
   if (price >= 500000) {
-    return Math.floor(price * 0.8);
+    ans *= 0.8;
   } else if (price >= 300000) {
-    return Math.floor(price * 0.9);
+    ans *= 0.9;
   } else if (price >= 100000) {
-    return Math.floor(price * 0.95)
-  } else {
-    return price;
+    ans *= 0.95;
   }
+  return parseInt(ans);
 }
