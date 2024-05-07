a94485697e7b2742726371bfb5b7f21c6884098d
zidoopal <167097136+heydoopal@users.noreply.github.com>
2024-05-07 13:23:22 +0900

    [level 0] Title: 가위 바위 보, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.js
@@ -0,0 +1,9 @@
+function solution(rsp) {
+  let ans = '';
+  for (let i = 0; i < rsp.length; i++) {
+    if (rsp[i] === '2') ans += '0';
+    else if (rsp[i] === '0') ans += '5';
+    else if (rsp[i] === '5') ans += '2';
+  }
+  return ans;
+}
