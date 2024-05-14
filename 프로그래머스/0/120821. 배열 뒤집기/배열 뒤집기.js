099e5674ac09dfb694da1d65587306bb610834f5
zidoopal <167097136+heydoopal@users.noreply.github.com>
2024-05-14 10:36:34 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -0,0 +1,9 @@
+const solution = num_list => num_list.reverse()
+
+// function solution(num_list) {
+//   let answer = new Array(num_list.length);
+//   for (let i = 0; i < num_list.length; i++) {
+//     answer[i] = num_list[num_list.length - 1 - i];
+//   }
+//   return answer;
+// }
