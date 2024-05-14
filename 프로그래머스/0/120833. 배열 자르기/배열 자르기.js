589bec1a1dc2cf4ef4554481a5146e91513f0acc
zidoopal <167097136+heydoopal@users.noreply.github.com>
2024-05-14 10:39:51 +0900

    [level 0] Title: 배열 자르기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.js
@@ -1,3 +1,7 @@
 function solution(numbers, num1, num2) {
-  return numbers.slice(num1, num2 +1);
+  let answer = [];
+  for (let i = num1; i < num2 + 1; i++) {
+    answer.push(numbers[i]);
+  }
+  return answer;
 }
