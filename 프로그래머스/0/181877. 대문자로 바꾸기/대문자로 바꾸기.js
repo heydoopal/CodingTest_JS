5842542f2c9ec003f9908fdd076cc146a60169da
zidoopal <167097136+heydoopal@users.noreply.github.com>
2024-05-15 15:18:08 +0900

    [level 0] Title: 대문자로 바꾸기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.js
+++ 프로그래머스/0/181877. 대문자로 바꾸기/대문자로 바꾸기.js
@@ -1,6 +1,3 @@
 function solution(myString) {
-  // myStrings에 있는 문자중 소문자면 toUpperCase 시켜버릐기?
-  return [...myString]
-    .map((char) => (char !== char.toUpperCase() ? char.toUpperCase() : char))
-    .join('');
+  return myString.toUpperCase()
 }
