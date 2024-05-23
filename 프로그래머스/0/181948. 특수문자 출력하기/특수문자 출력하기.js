6b13e2a9b184ab8d1e49680811aeb7749b548fb0
zidoopal <167097136+heydoopal@users.noreply.github.com>
2024-05-23 11:49:09 +0900

    [level 0] Title: 특수문자 출력하기, Time: 47.21 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.js
@@ -0,0 +1,10 @@
+const { log } = require('console');
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on('close', function () {
+  console.log(`!@#$%^&*(\\'"<>?:;`);
+});
