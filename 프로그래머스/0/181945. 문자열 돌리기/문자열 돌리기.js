2240ae0bd5330cf464fb38c428c85534af8ef94f
zidoopal <167097136+heydoopal@users.noreply.github.com>
2024-07-03 09:12:14 +0900

    [level 0] Title: 문자열 돌리기, Time: 45.17 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
+++ 프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.js
@@ -1,15 +1,14 @@
 const readline = require('readline');
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+    input: process.stdin,
+    output: process.stdout
 });
 
 let input = [];
 
 rl.on('line', function (line) {
-  const str = line;
-  for (let i = 0; i < str.length; i++) {
-    console.log(str[i]);
-  }
-  rl.close();
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    [...str].forEach(e => console.log(e))
 });
